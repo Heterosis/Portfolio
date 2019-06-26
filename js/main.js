4d0f1b543fcedeed06d0bf320484d83051d07c43
Heterosis <39578132+Heterosis@users.noreply.github.com>
2019-06-26 16:16:14 +0800

    scroll animation modified

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 AOS.init({
     duration: 750
 });
+
 $(".icon").hover(function () {
     $(this).addClass("animated tada");
 }, function () {
     $(this).removeClass("animated tada");
+});
+
+$("a[href^='#']").on("click", function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+
+    $("html, body").animate({
+        "scrollTop": $(target).offset().top - 56
+    });
 });
